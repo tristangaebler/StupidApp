5dba52b5a2b87f2294b250b5b8cddd4d00fd0bce
tristangaebler <tgae6529@740s-pl-115.local>
2015-12-11 08:57:02 -0700

    Initial Commit

---
+++ StupidApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StupidApp
+//
+//  Created by Gaebler, Tristan on 12/11/15.
+//  Copyright (c) 2015 Gaebler, Tristan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
